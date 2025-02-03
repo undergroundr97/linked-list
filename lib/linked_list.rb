cfd647f50829e45745fee555c07bb03565e0ca52
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-02-03 17:25:21 -0300

    a tail, head, prepend methods to linkedlist

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,21 +1,58 @@
 require_relative 'node'
 class LinkedList
-  attr_accessor :first, :next
-  def initialize(first = Node.new)
-    @first
+  attr_accessor :head
+  def initialize(node)
+    @head = Node.new(node)
+    @count = 1
   end
   def append(node)
-    app = Node.new
-    app.value = node
-    app.next_value = @first
+    current_node = @head
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+    end
+    current_node.next_node = Node.new(node, nil)
+    @count += 1
   end
-  
+  def prepend(node)
+     @head = Node.new(node, @head)
+     @count += 1
+  end
+  def size
+    @count
+  end
+  def head
+    @head.value
+  end
+  def tail
+    current_node = @head
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+    end
+    tail = current_node.value
+    tail
+  end
+  def at(index)
+    current_node = @head
+    if index > @count
+      nil
+    end
+  end
 end
 
-p link = LinkedList.new
+ link = LinkedList.new(2)
 
-p link.first
-p link.append('dog')
+ link.append('5')
+ link.append('54')
+ link.append('52')
+#  link.prepend('4')
+#  link.prepend('4')
+#  link.prepend('4')
+#  link.prepend('4')
+ link.append('52')
+ p link.size
 
-p link.first
-p link.dog
+#  p link.head
+
+p link.tail
+
+p link.at(10)
